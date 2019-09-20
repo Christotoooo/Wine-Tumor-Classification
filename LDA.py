136dc11e97a1ed874c75da790235baa6cb34a278
Johnny Huang <46142975+Lihui-Huang@users.noreply.github.com>
2019-09-20 16:03:20 -0400

    Create LDA.py
    Implemented the Linear Discriminative Analysis Algorithm

---
+++ LDA.py
@@ -0,0 +1,61 @@
+import pre_process
+import numpy as np
+import pandas as pd
+import csv
+from tqdm import tqdm
+import math
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+class LDA(object):
+    def __init__(self, X):
+        self.p = [0, 0]
+        self.u0 = []
+        self.u1 = []
+        self.sigma = np.zeros((len(X[0]), len(X[0])))
+
+    def fit(self, X, y):
+        zeros = np.count_nonzero(y == 0)
+        ones = np.count_nonzero(y == 1)
+        self.p[0] = float(zeros / (ones + zeros))
+        self.p[1] = float(ones / (ones + zeros))
+        for i in range(X.shape[1]):
+            self.u0.append(0)
+            self.u1.append(1)
+        """
+        for i in range(len(y)):
+            if y[i] == 0: 
+                for j in range(X.shape[1]):
+                    u0[j] += X[i][j]/p.count(0)
+            elif y[i] == 1:
+                for j in range(X.shape[1]):
+                    u1[j] += X[i][j]/p.count(1)
+        """
+        for i in range(len(y)):
+            if y[i] == 0:
+                #np.sum([self.u0, float(X[i]/zeros)],axis=0)
+                self.u0 += X[i]/ zeros
+            elif y[i] == 1:
+                #np.sum([self.u1,X[i]/ones],axis=0)
+                self.u1 += X[i] / ones
+        for i in range(len(y)):
+            if y[i] == 0:
+                self.sigma += (np.asmatrix((X[i] - self.u0)).transpose()).dot((np.asmatrix((X[i] - self.u0))))/(zeros + ones + 2)
+            elif y[i] == 1:
+                self.sigma += (np.asmatrix((X[i] - self.u1)).transpose()).dot((np.asmatrix((X[i] - self.u1))))/(zeros + ones + 2)
+            #print (self.sigma)
+
+    def predict(self, X):
+        y = []
+        for data in X:
+            logodds = math.log(self.p[1]/self.p[0])-0.5*np.dot(np.dot(self.u1, np.linalg.inv(self.sigma)),self.u1.transpose())+0.5*np.dot(np.dot(self.u0, np.linalg.inv(self.sigma)),self.u0.transpose())+np.dot(np.dot(data,np.linalg.inv(self.sigma)),(self.u1-self.u0).transpose())
+            if logodds < 0:
+                y.append(0)
+            else:
+                y.append(1)
+        return y
+
+
+
+
