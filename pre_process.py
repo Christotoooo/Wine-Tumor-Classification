f5ebd85a376a4a557b7bf7442e9239e1b9b5e68f
Christoto <christopherzheng0301@gmail.com>
2019-09-19 21:10:14 -0400

    (refactor) Updated the output format of pre_process_py into X.y

--- pre_process.py
+++ pre_process.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[60]:
-
-
 import numpy as np
 import pandas as pd
 import csv
@@ -15,62 +9,75 @@ import matplotlib.pyplot as plt
 # useful global variables
 wines_header = []
 tumors_header = []
+wines_global = []
+tumors_global = []
 
 
 def process_wines():
     with open("winequality-red.csv", 'r') as f:
         wines = list(csv.reader(f, delimiter=";"))
-    global wines_header 
+    global wines_header
     wines_header = np.array(wines[0])  # with label header
-    wines = np.array(wines[1:], dtype=np.float) # with label
-    
+    wines = np.array(wines[1:], dtype=np.float)  # with label
+
     # clean malinformed values by deleting the rows they inhabit
     invalid_index = []
     for i in range(len(wines)):
         for number in wines[i]:
             if math.isnan(number):
-                np.delete(wines,i,0)
-    
+                np.delete(wines, i, 0)
+
     # differentiate labels
-    for i in tqdm(range(len(wines[:,-1]))):
-        if wines[:,-1][i] >= 5:
-            wines[:,-1][i] = 1
+    for i in tqdm(range(len(wines[:, -1]))):
+        if wines[:, -1][i] >= 5:
+            wines[:, -1][i] = 1
         else:
-            wines[:,-1][i] = 0
-            
-    return wines
+            wines[:, -1][i] = 0
+
+    global wines_global
+    wines_global = wines
+    return wines[:, :-1], wines[:, -1]
+
 
 def process_tumors():
     with open("breast-cancer-wisconsin.data", 'r') as f:
         tumors = list(csv.reader(f, delimiter=";"))
-    
-    global tumors_header 
-    tumors_header = ["clump thickness","cell size","cell shape","marginal adhesion",                 "single epithelial cell size","number of bare nuclei","bland chromatin",                 "number of normal nuclei","mitosis","label"] # with label header but no IDs
-    
+
+    global tumors_header
+    tumors_header = ["clump thickness", "cell size", "cell shape", "marginal adhesion", \
+                     "single epithelial cell size", "number of bare nuclei", "bland chromatin", \
+                     "number of normal nuclei", "mitosis", "label"]  # with label header but no IDs
+
     # highlight malinformed values
     invalid_index = []
     for i in tqdm(range(len(tumors))):
         tumors[i] = tumors[i][0].split(",")
         for j in range(len(tumors[i])):
             if tumors[i][j].isnumeric() == False:
-                invalid_index.append(i)    #the whole row
-        #differentiate labels
+                invalid_index.append(i)  # the whole row
+        # differentiate labels
         if int(tumors[i][-1]) <= 2:
             tumors[i][-1] = '0'
         else:
             tumors[i][-1] = '1'
-    
+
     # clean malinformed values by deleting the rows they inhabit
     invalid_index.sort(reverse=True)
     for i in invalid_index:
         tumors.remove(tumors[i])
-       
-    tumors = np.array(tumors[0:],dtype=np.int)
-    tumors = tumors[:,1:]
-    return tumors
+
+    tumors = np.array(tumors[0:], dtype=np.int)
+    tumors = tumors[:, 1:]
+
+    global tumors_global
+    tumors_global = tumors
+
+    return tumors[:, :-1], tumors[:, -1]
+
 
 def stats_wines():
-    wines = process_wines()
+    global wines_global
+    wines = wines_global
     pos_index = []
     neg_index = []
     # separate two classes
@@ -81,21 +88,25 @@ def stats_wines():
             neg_index.append(index)
     pos_wines = wines[pos_index]
     neg_wines = wines[neg_index]
-    #sns.boxplot(data=pos_wines)
-    #sns.boxplot(data=neg_wines)
-    df = pd.DataFrame(wines,columns=wines_header)
+    # sns.boxplot(data=pos_wines)
+    # sns.boxplot(data=neg_wines)
+    df = pd.DataFrame(wines, columns=wines_header)
     df.describe()
-    #sns.pairplot(df) # density looks very suspicious
-    df_pos = pd.DataFrame(pos_wines,columns=wines_header)
+    # sns.pairplot(df) # density looks very suspicious
+    # sns_plot = sns.pairplot(df)
+    # sns_plot.savefig("wines_pairplot.png")
+    df_pos = pd.DataFrame(pos_wines, columns=wines_header)
     df_pos.describe()
-    df_neg = pd.DataFrame(neg_wines,columns=wines_header)
+    df_neg = pd.DataFrame(neg_wines, columns=wines_header)
     df_neg.describe()
 
+
 def stats_tumors():
-    tumors = process_tumors()
+    global tumors_global
+    tumors = tumors_global
     pos_index = []
     neg_index = []
-    #separate two classes
+    # separate two classes
     for index in range(len(tumors)):
         if tumors[index][-1] == 1:
             pos_index.append(index)
@@ -103,12 +114,11 @@ def stats_tumors():
             neg_index.append(index)
     pos_tumors = tumors[pos_index]
     neg_tumors = tumors[neg_index]
-    
-    df = pd.DataFrame(tumors,columns=tumors_header)
-    df.describe()
-    #sns.pairplot(df) # density looks very suspicious
-    df_pos = pd.DataFrame(pos_tumors,columns=tumors_header)
-    df_pos.describe()
-    df_neg = pd.DataFrame(neg_tumors,columns=tumors_header)
-    df_neg.describe()
 
+    df = pd.DataFrame(tumors, columns=tumors_header)
+    df.describe()
+    # sns.pairplot(df) # density looks very suspicious
+    df_pos = pd.DataFrame(pos_tumors, columns=tumors_header)
+    df_pos.describe()
+    df_neg = pd.DataFrame(neg_tumors, columns=tumors_header)
+    df_neg.describe()
