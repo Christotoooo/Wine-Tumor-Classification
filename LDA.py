ca52c1afb8e10a55c1e8bc226da61ae86502d9a2
Johnny Huang <46142975+Lihui-Huang@users.noreply.github.com>
2019-09-22 20:07:43 -0400

    Update LDA.py
    Updated Linear Discriminant Analysis

--- LDA.py
+++ LDA.py
@@ -16,27 +16,30 @@ class LDA(object):
         self.sigma = np.zeros((len(X[0]), len(X[0])))
 
     def fit(self, X, y):
-        zeros = np.count_nonzero(y == 0)
-        ones = np.count_nonzero(y == 1)
-        self.p[0] = float(zeros / (ones + zeros))
-        self.p[1] = float(ones / (ones + zeros))
+        self.p[0] = np.count_nonzero(y == 0)
+        self.p[1] = np.count_nonzero(y == 1)
         for i in range(len(y)):
             if y[i] == 0:
-                self.u0 += X[i]/ zeros
+                self.u0 = np.add(X[i], self.u0)
             elif y[i] == 1:
-                self.u1 += X[i] / ones
+                self.u1 = np.add(X[i], self.u1)
+        self.u0 = np.divide(self.u0, self.p[0])
+        self.u1 = np.divide(self.u1, self.p[1])
         for i in range(len(y)):
             if y[i] == 0:
-                self.sigma += np.outer((np.asmatrix((X[i] - self.u0))),((np.asmatrix((X[i] - self.u0))).transpose()))/(zeros + ones - 2)
+                self.sigma = np.add(self.sigma, np.outer((X[i] - self.u0), (X[i] - self.u0).transpose()))
             elif y[i] == 1:
-                #self.sigma += (np.asmatrix((X[i] - self.u1))).dot((np.asmatrix((X[i] - self.u1))).transpose())/(zeros + ones - 2)
-                self.sigma += np.outer((np.asmatrix((X[i] - self.u1))), ((np.asmatrix((X[i] - self.u1))).transpose())) / (
-                            zeros + ones - 2)
+                self.sigma = np.add(self.sigma, np.outer((X[i] - self.u1), (X[i] - self.u1).transpose()))
+        self.sigma = np.divide(self.sigma, (self.p[0] + self.p[1] - 2))
 
     def predict(self, X):
         y = []
+        w0 = math.log(self.p[1]/self.p[0])
+        w0 -= 0.5*np.dot(np.dot(self.u1.transpose(), np.linalg.inv(self.sigma)), self.u1)
+        w0 += 0.5*np.dot(np.dot(self.u0.transpose(), np.linalg.inv(self.sigma)), self.u0)
         for data in X:
-            logodds = math.log(self.p[1]/self.p[0])-0.5*np.dot(np.dot(self.u1.transpose(), np.linalg.inv(self.sigma)),self.u1)+0.5*np.dot(np.dot(self.u0.transpose(), np.linalg.inv(self.sigma)),self.u0)+np.dot(np.dot(data.transpose(),np.linalg.inv(self.sigma)),(self.u1-self.u0))
+            w1 = np.dot(np.dot(data.transpose(), np.linalg.inv(self.sigma)), (self.u1 - self.u0))
+            logodds = w0 + w1
             if logodds <= 0:
                 y.append(0)
             else:
